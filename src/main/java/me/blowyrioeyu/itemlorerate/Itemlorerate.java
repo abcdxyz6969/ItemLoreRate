f126d42801a3d657aff47dd897e0b699098b6eec
abcdxyz6969 <rioeyu@4h4b.love>
2025-03-29 15:36:52 +0700

    try

---
+++ src/main/java/me/blowyrioeyu/itemlorerate/Itemlorerate.java
@@ -0,0 +1,17 @@
+package me.blowyrioeyu.itemlorerate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Itemlorerate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
